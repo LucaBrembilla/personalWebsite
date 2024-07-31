dc52b13265e307027d5783a496a21e56b3c9e201
LucaBrembilla <lucabremby00@gmail.com>
2024-07-31 14:30:26 +0200

    Update

--- personal-website/src/App.js
+++ personal-website/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
-import Main from './components/Main';
+import Main from './home/Main';
 import Footer from './components/Footer';
-import Home from './Home';
+import About from './about/About';
+import Contact from './contact/Contact';
 import './App.css';
 
 const App = () => {
@@ -12,6 +13,8 @@ const App = () => {
             <Header/>
             <Routes>
                 <Route path="/" element={<Main/>}/>
+                <Route path="/about" element={<About/>}/>
+                <Route path="/contact" element={<Contact/>}/>
                 {/* Define other routes here as needed */}
             </Routes>
             <Footer/>
