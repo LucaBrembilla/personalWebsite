5236cd4c7b9035996d557f49343c15d7d290dd56
LucaBrembilla <lucabremby00@gmail.com>
2024-07-31 10:17:54 +0200

    Update Home

--- personal-website/src/components/Header.js
+++ personal-website/src/components/Header.js
@@ -8,10 +8,8 @@ const Header = () => {
             <nav>
                 <ul>
                     <li><a href="#home">Home</a></li>
-                    <li><a href="#services">Services</a></li>
-                    <li><a href="#about">About</a></li>
-                    <li><a href="#skills">Skills</a></li>
-                    <li><a href="#portfolio">Portfolio</a></li>
+                    <li><a href="#about">Who am I</a></li>
+                    <li><a href="#computerScience">Computer Science</a></li>
                     <li><a href="#blog">Blog</a></li>
                     <li><a href="#contact">Contact</a></li>
                 </ul>
--- personal-website/src/components/Main.js
+++ personal-website/src/components/Main.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebookF, faTwitter, faLinkedinIn, faInstagram, faBehance } from '@fortawesome/free-brands-svg-icons';
+import {  faLinkedinIn, faInstagram, faGithub, faXTwitter } from '@fortawesome/free-brands-svg-icons';
 import '../App.css';
 
 const Main = () => {
     return (
         <main className="main-content">
             <div className="intro">
-                <h1>I am Luca Brembilla</h1>
-                <p>I'm Luca, software engineer and much more (I hope).</p>
+                <p className="intro-text">Hello, I am</p>
+                <h1 className="intro-name">Luca Brembilla</h1>
+                <p className="intro-title">I'm Luca, software engineer (and much more, I hope).</p>
                 <button className="portfolio-btn">My Portfolio</button>
             </div>
             <div className="social-icons">
-                <a href="#" className="social-icon"><FontAwesomeIcon icon={faFacebookF}/></a>
-                <a href="#" className="social-icon"><FontAwesomeIcon icon={faTwitter}/></a>
-                <a href="#" className="social-icon"><FontAwesomeIcon icon={faLinkedinIn}/></a>
-                <a href="#" className="social-icon"><FontAwesomeIcon icon={faInstagram}/></a>
-                <a href="#" className="social-icon"><FontAwesomeIcon icon={faBehance}/></a>
+                <a href="https://www.linkedin.com/in/lucabrembilla/" target="_blank" className="social-icon"><FontAwesomeIcon icon={faLinkedinIn}/></a>
+                <a href="https://www.instagram.com/il_breee" target="_blank" className="social-icon"><FontAwesomeIcon icon={faInstagram}/></a>
+                <a href="https://github.com/LucaBrembilla" target="_blank" className="social-icon"><FontAwesomeIcon icon={faGithub}/></a>
+                <a href="https://x.com/LucaBrembillaa" target="_blank" className="social-icon"><FontAwesomeIcon icon={faXTwitter}/></a>
             </div>
         </main>
     );
