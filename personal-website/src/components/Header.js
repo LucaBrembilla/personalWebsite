bbe0c3f84f734cb17767cc90ba4a3feb447a6eb0
LucaBrembilla <lucabremby00@gmail.com>
2024-07-31 18:55:52 +0200

    Update

--- personal-website/src/components/Header.js
+++ personal-website/src/components/Header.js
@@ -1,31 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css';
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faGithub, faInstagram, faLinkedinIn, faXTwitter} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faInstagram, faLinkedinIn, faXTwitter } from "@fortawesome/free-brands-svg-icons";
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 
 const Header = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
     return (
         <header className="header">
-            <div className="logo">Bre</div>
-            <div className="social-icons">
-                <a href="https://www.linkedin.com/in/lucabrembilla/" target="_blank"
-                   className="social-icon"><FontAwesomeIcon icon={faLinkedinIn}/></a>
-                <a href="https://www.instagram.com/il_breee" target="_blank" className="social-icon"><FontAwesomeIcon
-                    icon={faInstagram}/></a>
-                <a href="https://github.com/LucaBrembilla" target="_blank" className="social-icon"><FontAwesomeIcon
-                    icon={faGithub}/></a>
-                <a href="https://x.com/LucaBrembillaa" target="_blank" className="social-icon"><FontAwesomeIcon
-                    icon={faXTwitter}/></a>
+            <div className="left-header">
+                <div className="logo">Bre</div>
+                <div className="social-icons">
+                    <a href="https://www.linkedin.com/in/lucabrembilla/" target="_blank"
+                       className="social-icon"><FontAwesomeIcon icon={faLinkedinIn}/></a>
+                    <a href="https://www.instagram.com/il_breee" target="_blank" className="social-icon"><FontAwesomeIcon
+                        icon={faInstagram}/></a>
+                    <a href="https://github.com/LucaBrembilla" target="_blank" className="social-icon"><FontAwesomeIcon
+                        icon={faGithub}/></a>
+                    <a href="https://x.com/LucaBrembillaa" target="_blank" className="social-icon"><FontAwesomeIcon
+                        icon={faXTwitter}/></a>
+                </div>
             </div>
-            <nav>
+            <div className="menu-toggle" onClick={toggleMenu}>
+                <FontAwesomeIcon icon={isOpen ? faTimes : faBars} />
+            </div>
+            <nav className={isOpen ? 'open' : ''}>
                 <ul>
                     <li><Link to="/">Home</Link></li>
-                    <li><a href="about">Who am I</a></li>
-                    <li><a href="computerscience">Computer Science</a></li>
-                    <li><a href="radiomusic">Radio and Music</a></li>
-                    <li><a href="art">Art</a></li>
-                    <li><a href="contact">Contact</a></li>
+                    <li><a href="/about">Who am I</a></li>
+                    <li><a href="/computerscience">Computer Science</a></li>
+                    <li><a href="/radiomusic">Radio and Music</a></li>
+                    <li><a href="/art">Art</a></li>
+                    <li><a href="/contact">Contact</a></li>
                 </ul>
             </nav>
         </header>
