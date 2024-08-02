5c6d7f73549a0c491361b26f389dff5bcb978e45
LucaBrembilla <lucabremby00@gmail.com>
2024-08-02 21:43:44 +0200

    Update

--- personal-website/src/home/Main.js
+++ personal-website/src/home/Main.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import './Main.css';
 import profileImage from '../images/me.jpg';
+import resume from '../docs/resume.pdf';
 
 const Main = () => {
+    const handleResumeDownload = () => {
+        const link = document.createElement('a');
+        link.href = resume; // The path to the PDF file
+        link.download = 'Luca_Brembilla_Resume.pdf'; // The filename for the downloaded file
+        link.click();
+    };
+
     return (
         <main className="main-content">
             <div className="intro">
@@ -11,11 +19,11 @@ const Main = () => {
                     <h1 className="intro-name">Luca Brembilla</h1>
                     <p className="intro-title">I'm Luca, a software engineer.</p>
                     <p className="intro-title">And much more, I hope.</p>
-                    <button className="resume-btn">My Resume</button>
+                    <button className="resume-btn" onClick={handleResumeDownload}>Download Resume</button>
                 </div>
             </div>
             <div className="image-container">
-                <img src={profileImage} alt="Luca Brembilla" className="profile-image"/>
+            <img src={profileImage} alt="Luca Brembilla" className="profile-image"/>
             </div>
         </main>
     );
