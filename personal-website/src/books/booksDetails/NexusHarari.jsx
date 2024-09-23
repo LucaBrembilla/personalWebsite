aff97caf0c25385ae9e40e445c5946b3f3da86da
LucaBrembilla <lucabremby00@gmail.com>
2024-09-23 09:28:31 +0200

    Update

--- personal-website/src/books/booksDetails/NexusHarari.jsx
+++ personal-website/src/books/booksDetails/NexusHarari.jsx
@@ -15,51 +15,51 @@ const NexusHarari = () => (
                 you can't miss his bestseller, Sapiens: A Brief History of Humankind, which remains a standout.
             </p>
             <h3>About the Author</h3>
-                <p>
-                    Prof. Yuval Noah Harari is a historian, philosopher and the global bestselling
-                    author of "Sapiens: A Brief History of Humankind", "Homo Deus: A Brief History of Tomorrow",
-                    "21 Lessons for the 21st Century". His books have sold over 45 million copies in 65 languages,
-                    with Sapiens alone selling 25 million copies since it was first published in 2013.
-                    A New York Times and Sunday Times #1 bestseller, Sapiens spent an incredible 96 consecutive
-                    weeks in the top 3 of the Sunday Times bestseller list. Yuval Noah Harari is also behind
-                    Sapienship – an international social impact company focused on education and storytelling,
-                    which he co-founded with his husband Itzik Yahav. Harari is considered one of the world’s
-                    most influential public intellectuals today.
-                </p>
+            <p>
+                Prof. Yuval Noah Harari is a historian, philosopher and the global bestselling
+                author of "Sapiens: A Brief History of Humankind", "Homo Deus: A Brief History of Tomorrow",
+                "21 Lessons for the 21st Century". His books have sold over 45 million copies in 65 languages,
+                with Sapiens alone selling 25 million copies since it was first published in 2013.
+                A New York Times and Sunday Times #1 bestseller, Sapiens spent an incredible 96 consecutive
+                weeks in the top 3 of the Sunday Times bestseller list. Yuval Noah Harari is also behind
+                Sapienship – an international social impact company focused on education and storytelling,
+                which he co-founded with his husband Itzik Yahav. Harari is considered one of the world’s
+                most influential public intellectuals today.
+            </p>
             <h2>Book Summary</h2>
             <h3>Prologue</h3>
-                <p>
-                    In the prologue the subject of the book is presented. Harari begin by what he had previously
-                    stated in his books: the idea that large human societies are held together by shared stories,
-                    religions, and ideologies, which allowed humans to cooperate in larger groups than any
-                    other species. Humans now have accumulated a lot of information and power. However, that
-                    didn't give us answers to the big questions of life and what is our purpose. </p>
-                <p>
-                    The naive view of information is that it brings truth, which in turn brings wisdom and power. So,
-                    it holds the Google statement "to organize the world's information and make it universally
-                    accessible and useful".
-                </p>
+            <p>
+                In the prologue the subject of the book is presented. Harari begin by what he had previously
+                stated in his books: the idea that large human societies are held together by shared stories,
+                religions, and ideologies, which allowed humans to cooperate in larger groups than any
+                other species. Humans now have accumulated a lot of information and power. However, that
+                didn't give us answers to the big questions of life and what is our purpose. </p>
+            <p>
+                The naive view of information is that it brings truth, which in turn brings wisdom and power. So,
+                it holds the Google statement "to organize the world's information and make it universally
+                accessible and useful".
+            </p>
             <h3>Chapter I - What is information</h3>
-                <p>
-                    The naive view of information considers it as everything that people can use to discover the truth.
-                    This view links information to truth,it is a way to represent reality.
-                    However, this book holds that most information is not an attempt to represent reality but
-                    something else.
-                </p>
-                <p>
-                    What is truth? It is something that accurately represent certain aspects of reality.
-                    One condition for truth is universalism: there exists only one reality. However, it is complex.
-                    Information is a representation of reality, which can be true or false. Surely is inaccurate: we
-                    should have a 1:1 map to have 100% accuracy, as FS blog reminds that <a
-                    href="https://fs.blog/map-and-territory/" target="_blank">The map is not the territory</a>.
-                    Information has the role of create new realities by tying together disparate things.
-                    It is a social nexus.
-                </p>
-                <p>
+            <p>
+                The naive view of information considers it as everything that people can use to discover the truth.
+                This view links information to truth,it is a way to represent reality.
+                However, this book holds that most information is not an attempt to represent reality but
+                something else.
+            </p>
+            <p>
+                What is truth? It is something that accurately represent certain aspects of reality.
+                One condition for truth is universalism: there exists only one reality. However, it is complex.
+                Information is a representation of reality, which can be true or false. Surely is inaccurate: we
+                should have a 1:1 map to have 100% accuracy, as FS blog reminds that <a
+                href="https://fs.blog/map-and-territory/" target="_blank">The map is not the territory</a>.
+                Information has the role of create new realities by tying together disparate things.
+                It is a social nexus.
+            </p>
+            <p>
                     <span className="citation">"Homo sapiens didn't conquer the world because we are talented at turning
                     information into an accurate map of reality. Rather, the secret of our success is that we are
                     talented at using information to connect lots of individuals."</span>
-                </p>
+            </p>
             <h3>
                 Chapter II - Stories: Unlimited Connections
             </h3>
@@ -103,7 +103,17 @@ const NexusHarari = () => (
             <p>
                 If humans are prone to error, how can we trust self-correcting mechanisms? Religion often removed humans
                 from the equation, portraying God as infallible. Yet, this ultimately led to relying on the judgment of
-                certain individuals. In this sense, books are technologies to bypass human fallibility.
+                certain individuals. In this sense, books are technologies to bypass human fallibility, but failed at that.
+                The history of witch hunts demonstrates that it is not more information that lead to truth. The driving force
+                of the scientific revolution was the self-correcting mechanism of scientific institutions, and not the
+                invention of printer.
+                <span className="citation">What makes scientific self-correcting mechanisms particularly strong is that
+		scientific institutions are not just willing to admit institutional error and ignorance; they are actively
+		seeking to expose them.</span>
+                This can be viewed also in the "publish or perish" principle: who brings innovation is awarded.
+                The flow of information balances the trade-off between truth and order. Self-correcting mechanism of scientific
+                institutions was possible because they leave the job of preserving the social order to other insitutions. Can these
+                other institutions have a self-correcting mechanism?
             </p>
         </div>
     </main>
